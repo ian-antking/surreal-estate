4d8dff1c386c4a2c40b6c30eb1c849a98582487b
Ian King <ian.antking@gmail.com>
2018-11-01 17:58:00 +0000

    Set up project repository

---
+++ src/components/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/app.scss';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
+
+export default App;
